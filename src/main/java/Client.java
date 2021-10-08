633b9a1c3b2085e003c99f5731208eb8f8ff53a8
Ivanova Irina <ivanovairishav@ya.ru>
2021-10-08 12:30:35 +0300

    correction 1

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -6,19 +6,17 @@ import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) throws Exception {
-    String host = "localhost";
-    int port = 8089;
-    try (Socket clientSocket = new Socket(host, port);
-         PrintWriter out = new
-            PrintWriter(clientSocket.getOutputStream(), true);
-         BufferedReader in = new BufferedReader(new
-                 InputStreamReader(clientSocket.getInputStream()))) {
+        String host = "localhost";
+        int port = 8089;
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
 
-        out.println("Irisha\n");
+            out.println("Irisha\n");
 
-        String resp = in.readLine();
-        System.out.println(resp);
-    }
+            String resp = in.readLine();
+            System.out.println(resp);
+        }
         InetAddress inetAddress = InetAddress.getByName(host);
         System.out.println(host + ", ip address: " +
                 inetAddress.getHostAddress());
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -6,21 +6,21 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
-        System.out.println("Server startup");
+    public static void main(String[] args) {
         int port = 8089;
-        while (true) {
-            ServerSocket serverSocket = new ServerSocket(port);
-            Socket clientS0cket = serverSocket.accept();
-            PrintWriter out = new PrintWriter(clientS0cket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientS0cket.getInputStream()));
+            try (ServerSocket serverSocket = new ServerSocket(port);
+                 Socket clientSocket = serverSocket.accept();
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
 
-            System.out.printf("New connection accepted, Port: %d\n", clientS0cket.getPort());
+                System.out.println("New connection accepted");
 
-            final String name = in.readLine();
+                final String name = in.readLine();
 
-            out.println(String.format("Hi %s, your port is %d", name, clientS0cket.getPort()));
-            serverSocket.close();
+                out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
-}
